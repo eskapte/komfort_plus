94a20e366f389bbf426b49e207346bae50cdf4e3
eskapte <eskapte@mail.ru>
2021-10-31 22:19:22 +0300

    finish

---
+++ main.js
@@ -0,0 +1,78 @@
+"use strict"
+
+const showMoreBtn = document.querySelector("#show-more-btn");
+const hiddenCategories = document.querySelectorAll(".category:nth-last-child(-n + 7)");
+const hiddenImages = document.querySelectorAll("img[data-src]");
+const categoriesBlock = document.querySelector(".work-photos");
+const upBtn = document.querySelector("#up-btn");
+const navBtns = document.querySelectorAll(".main-nav a");
+const menuBurger = document.querySelector(".menu-burger");
+const mainNav = document.querySelector("nav.main-nav");
+const headerContacts = document.querySelector(".header-contacts");
+
+let   isOpen = false;
+let   isFirst = true;
+
+showMoreBtn.addEventListener("click", evt => {
+
+    if (isFirst)
+        hiddenImages.forEach(img => img.setAttribute("src", img.getAttribute("data-src")));
+    isFirst = false;
+    hiddenCategories.forEach(category => category.classList.toggle("hidden"));
+
+    showMoreBtn.classList.toggle("active");
+    isOpen = !isOpen;
+    showMoreBtn.textContent = isOpen ? "Скрыть" : "Показать больше";
+
+});
+
+const myCarousel = new Carousel(document.querySelector(".carousel"), {
+    'slidesPerPage' : 1,
+    'preload' : '2',
+  });
+
+window.addEventListener("scroll", evt => {
+    if (window.scrollY > 580)
+        upBtn.style.display = 'block';
+    else
+        upBtn.style.display = 'none';
+});
+
+// scroll to the top
+upBtn.addEventListener("click", evt => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+// menu burger
+menuBurger.addEventListener("click", evt => {
+    mainNav.classList.toggle("nav-show");
+    headerContacts.classList.toggle("contacts-show");
+    document.querySelector("body").classList.toggle("no-scroll");
+});
+
+// goto to sections
+navBtns.forEach(btn => {
+    btn.addEventListener("click", evt => {
+        evt.preventDefault();
+        const navLink = evt.target;
+        if (navLink.dataset.goto && document.querySelector(navLink.dataset.goto)) {
+            const gotoSection = document.querySelector(navLink.dataset.goto);
+            const gotoBlockValue = gotoSection.getBoundingClientRect().top + pageYOffset;
+            console.log(gotoBlockValue);
+
+            // close the menu
+            document.querySelector("body").classList.toggle("no-scroll");
+            mainNav.classList.toggle("nav-show");
+            headerContacts.classList.toggle("contacts-show");
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+        }
+    })
+})
+
